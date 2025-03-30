02a331aa7091707c29f82e177dbb1a5c6a0b997e
allbek <135400458+allbek@users.noreply.github.com>
2025-03-30 15:34:16 +0500

    Описание изменений

--- main/settings.py
+++ main/settings.py
@@ -121,11 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Сюда Django соберет все файлы
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "universities/static"),  # Папка, где хранятся исходные стили
-]
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # Если у тебя есть статические файлы в папке проекта
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Папка, куда Django соберет все статики
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
